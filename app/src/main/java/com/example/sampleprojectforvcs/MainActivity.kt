d9355a78f863f24354e826d0645b58d001f22216
Shweta Vashi <shweta.vashi@uffizio.in>
2020-11-24 17:03:34 +0530

    Initial Commit

--- app/src/main/java/com/example/sampleprojectforvcs/MainActivity.kt
+++ app/src/main/java/com/example/sampleprojectforvcs/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.sampleprojectforvcs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        Log.d("Hello", "Heyy")
     }
 }
